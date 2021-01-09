fcd211cdc04b52b60636d452c565fd94486e16bb
thePwnerer <maxpwong@gmail.com>
2021-01-09 15:53:25 -0800

    didnt understand how to do this well. Will need to work on this more

--- 1-8-21/skeleton/lib/00_tree_node.rb
+++ 1-8-21/skeleton/lib/00_tree_node.rb
@@ -22,7 +22,59 @@ class PolyTreeNode
             end
         else
             @parent = nil
-            p "Please enter a parent value other than nil!"
         end
     end
+
+    def add_child(child)
+       @children << child
+       child.parent = self
+    end
+
+    def remove_child(child)
+        if child && !self.children.include?(child)
+            raise "Tried to remove node that isn't a child"
+        end
+        child.parent = nil
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+        self.children.each do |child|
+            result = child.dfs(target_value)
+            return result unless result.nil?
+        end
+
+        nil
+    end
+=begin
+    def dfs(target = nil, &prc)
+        raise "Need a proc or target" if [target, prc].none?
+        prc ||= Proc.new { |node| node.value == target }
+
+        return self if prc.call(self)
+
+        children.each do |child|
+        result = child.dfs(&prc)
+        return result unless result.nil?
+        end
+
+        nil
+    end
+=end
+
+    def bfs(target = nil, &prc)
+        raise "Need a proc or target" if [target, prc].none?
+        prc ||= Proc.new { |node| node.value == target }
+
+        nodes = [self]
+        until nodes.empty?
+        node = nodes.shift
+
+        return node if prc.call(node)
+        nodes.concat(node.children)
+        end
+
+        nil
+    end
+
 end
